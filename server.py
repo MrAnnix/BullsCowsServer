441c7429365f7e8af66566b41b03146795742a8c
Raúl San Martín Aniceto <hola@raulsanmartin.es>
2018-10-28 20:48:06 +0100

    Removed Comm

--- server.py
+++ server.py
@@ -38,6 +38,28 @@ class Server(object):
 
         self.server.close()
 
+    def send(channel, *args):
+        buf = marshall(args)
+        value = socket.htonl(len(buf))
+        size = struct.pack("L", value)
+        channel.send(size)
+        channel.send(buf)
+
+    def receive(channel):
+        size = struct.calcsize("L")
+        size = channel.recv(size)
+        try:
+            size = socket.ntohl(struct.unpack("L", size)[0])
+        except:
+            return ''
+
+        buf = ""
+
+        while len(buf) < size:
+            buf = channel.recv(size - len(buf))
+
+        return unmarshall(buf)[0]
+
     def login(self, client):
         info = self.clientinfo[client]
         host, id = info[0][0], info[1]
