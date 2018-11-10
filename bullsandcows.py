2c226990ff3ce895d603af875085693baca9a31d
Raúl San Martín Aniceto <hola@raulsanmartin.es>
2018-11-10 12:40:07 +0100

    BullsAndCows class done

--- bullsandcows.py
+++ bullsandcows.py
@@ -4,5 +4,13 @@ class BullsAndCows():
     def __init__(self):
         self.__secret = ''.join(random.choice(string.digits) for _ in range(4))
 
-    def guess(self):
-        return 0
+    def compare(self, guess):
+        if len(guess) != len(self.__secret):
+            return 'Error, the guess length has to be ' + len(self.__secret)
+        bullscows = [0, 0]
+        for i in range(len(self.__secret)):
+            if guess[i] == self.__secret[i]:
+                bullscows[0]+=1;
+            elif guess[i] in self.__secret:
+                bullscows[1]+=1;
+        return bullscows
